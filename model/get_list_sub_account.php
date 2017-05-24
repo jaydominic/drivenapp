d72f27df8104efa9fae771b64609be88b997cf89
X71108 <X71108@192.168.1.105>
2017-05-24 23:37:18 +0800

    minor code cleanup

--- model/get_list_sub_account.php
+++ model/get_list_sub_account.php
@@ -16,7 +16,7 @@ function get_list_sub_account($dbconn, $parent_account = NULL) {
 	
 	if ($parent_account != NULL) {
 	
-		$strsql = "SELECT chart_of_account_sub_account, chart_of_account_parent_account, " . 
+		$strsql = "SELECT chart_of_account_id, chart_of_account_sub_account, chart_of_account_parent_account, " . 
 			"chart_of_account_account_no, chart_of_account_group, chart_of_account_type " .
 			"FROM tbl_chart_of_account USE INDEX (sub_account) " .
 			"WHERE chart_of_account_parent_account = ? AND chart_of_account_is_active = 'Y' " .
@@ -29,7 +29,7 @@ function get_list_sub_account($dbconn, $parent_account = NULL) {
 	
 	} else {
 		
-		$strsql = "SELECT chart_of_account_sub_account, chart_of_account_parent_account, " .
+		$strsql = "SELECT chart_of_account_id, chart_of_account_sub_account, chart_of_account_parent_account, " .
 			"chart_of_account_account_no, chart_of_account_group, chart_of_account_type " .
 			"FROM tbl_chart_of_account USE INDEX (sub_account) " .
 			"WHERE chart_of_account_is_active = 'Y' AND chart_of_account_mark_as_deleted IS NULL " .
@@ -43,10 +43,11 @@ function get_list_sub_account($dbconn, $parent_account = NULL) {
 	
 	$stmt->execute();
 	
-	$stmt->bind_result($coa_sub_acct, $coa_parent_acct, $coa_acct_no, $coa_group, $coa_type);
+	$stmt->bind_result($coa_id, $coa_sub_acct, $coa_parent_acct, $coa_acct_no, $coa_group, $coa_type);
 	
 	while($stmt->fetch()) {
-		$resultarray[] = array('coa_sub_account' => $coa_sub_acct,
+		$resultarray[] = array('coa_id' => $coa_id,
+							'coa_sub_account' => $coa_sub_acct,
 							'coa_parent_account' => $coa_parent_acct,
 							'coa_account_no' => $coa_acct_no,
 							'coa_group' => $coa_group,
